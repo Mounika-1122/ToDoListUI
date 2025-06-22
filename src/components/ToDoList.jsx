fc7669f58f6b41d3d4f181b70156723291fd9861
Rankireddy <akarsh.1@iitj.ac.in>
2025-06-22 11:12:49 +0530

    changed css

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -35,31 +35,31 @@ export default function ToDoList() {
         
     }
   return (
-    <div className='flex items-center flex-col m-[100px] '>
-        <h1 className='font-bold text-3xl mb-3 text-[#FF9898]'>TO-DO-LIST</h1>
+    <div className='flex items-center flex-col m-[100px]'>
+        <h1 className='font-bold text-3xl mb-3 text-white'>TO-DO-LIST</h1>
         <div>
-            <input className='outline-none ml-3 border-[#FF9898] border-[3px] rounded-sm text-gray-400 text-lg font-bold pl-2 py-[2px] ' 
+            <input className='outline-none ml-3 border-gray-600 border-2 rounded-sm text-[#fefbf6eb] text-lg font-medium pl-2 py-[2px] bg-transparent transition ease-out duration-500 hover hover:bg-[#1773a0] ' 
                 type="text"
                 placeholder='Enter a task...'
                 value={newTask}
                 onChange={ handleInputChange}/>
-                <button className='ml-3 border-[#FF9898] border-[3px] rounded-sm text-gray-400 text-md font-bold px-3 transition ease-out duration-500 hover hover:text-[#FFAAAA]'
+                <button className='ml-3 border-gray-600 border-2 rounded-sm text-[#fefbf6eb] text-md font-bold px-3 bg-transparent transition ease-out duration-500 hover hover:bg-[#1773a0] '
                 onClick={addTask}
                 >Add</button>
         </div>
         <ol className=''>
             {tasks.map((task, index)=>
-                <li key={index} className='p-2 m-2 flex items-center justify-center ' >
+                <li key={index} className='p-2 m-2 flex items-center justify-center text-[#fefbf6eb] ' >
                     <span className='flex-1'>{index+1}. {task}</span>
-                    <button className='ml-3  border-[#FF9898] border-[3px] px-2  rounded-sm text-gray-400 text-md font-bold transition ease-out duration-500 hover hover:text-[#FFAAAA] '
+                    <button className='ml-3   px-2  border-gray-600 border-2 rounded-sm text-[#fefbf6eb] text-lg font-sm pl-2 py-[2px] bg-transparent transition ease-out duration-500 hover hover:bg-[#1773a0] '
                     onClick={()=>deleteTask(index)}
                     >Delete</button>
 
-                    <button className='ml-3 border-[#FF9898] border-[3px] px-2  rounded-sm text-gray-400 text-md font-bold'
+                    <button className='ml-3 border-gray-600 border-2 rounded-sm text-[#fefbf6eb] text-lg font-sm pl-2 p-[2px] bg-transparent transition ease-out duration-500 hover hover:bg-[#1773a0]'
                     onClick={()=>moveTaskUp(index)}
                     >☝🏻</button>
 
-                    <button className='ml-3 border-[#FF9898] border-[3px] px-2  rounded-sm text-gray-400 text-md font-bold'
+                    <button className='ml-3 border-gray-600 border-2 rounded-sm text-[#fefbf6eb] text-lg font-sm pl-2 p-[2px] bg-transparent transition ease-out duration-500 hover hover:bg-[#1773a0]'
                     onClick={()=>moveTaskDown(index)}
                     >👇🏻</button>
                 </li>
--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -3,7 +3,7 @@ import ToDoList from '../components/ToDoList'
 
 export default function IntroPage() {
   return (
-    <div className='w-full h-full pl-2 pt-2'>
+    <div className='w-full h-full pl-2 pt-2 '>
         <ToDoList />
     </div>
   );
